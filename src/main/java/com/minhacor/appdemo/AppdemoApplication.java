7ceab08f3aff0120ea77362b68b33af83e354b71
Andre Mendonca <andremendoncaolv@gmail.com>
2020-10-26 12:03:09 -0300

    Criando operacao de instanciacao

--- src/main/java/com/minhacor/appdemo/AppdemoApplication.java
+++ src/main/java/com/minhacor/appdemo/AppdemoApplication.java
@@ -1,13 +1,31 @@
 package com.minhacor.appdemo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.minhacor.appdemo.domain.Categoria;
+import com.minhacor.appdemo.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class AppdemoApplication {
+public class AppdemoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppdemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
