6e82529010a6ca24a25d3f93599e13962702c1e8
Andre Mendonca <andremendoncaolv@gmail.com>
2020-10-27 14:12:34 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/minhacor/appdemo/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.minhacor.appdemo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.minhacor.appdemo.domain.Pedido;
+import com.minhacor.appdemo.repositories.PedidoRepository;
+import com.minhacor.appdemo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository categoriaRepository;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> categoria = categoriaRepository.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
+	}
+}
