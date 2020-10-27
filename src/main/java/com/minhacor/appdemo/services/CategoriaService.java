56ca61e9596b606dabf9af1bc8d98433d6fd1ffc
Andre Mendonca <andremendoncaolv@gmail.com>
2020-10-27 18:25:58 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/minhacor/appdemo/services/CategoriaService.java
+++ src/main/java/com/minhacor/appdemo/services/CategoriaService.java
@@ -3,8 +3,12 @@ package com.minhacor.appdemo.services;
 import java.util.List;
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.minhacor.appdemo.domain.Categoria;
@@ -44,6 +48,11 @@ public class CategoriaService {
 	}
 	
 	public List<Categoria> findAll(){
-		return repository.findAll();
+		return repository.findAll(); 
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
