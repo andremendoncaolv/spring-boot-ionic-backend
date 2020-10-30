da4588cf437a4057b3f5dc3774de8a011067f37c
Andre Mendonca <andremendoncaolv@gmail.com>
2020-10-30 09:35:36 -0300

    Validacao customizada: email nao repetido nna insercao de Cliente

--- src/main/java/com/minhacor/appdemo/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/minhacor/appdemo/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.minhacor.appdemo.domain.Cliente;
 import com.minhacor.appdemo.domain.enums.TipoCliente;
 import com.minhacor.appdemo.dto.ClienteNewDTO;
+import com.minhacor.appdemo.repositories.ClienteRepository;
 import com.minhacor.appdemo.resources.exception.FieldMessage;
 import com.minhacor.appdemo.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldMessage("email", "Email já exixtente"));
 		}
 
 		for (FieldMessage e : list) {
